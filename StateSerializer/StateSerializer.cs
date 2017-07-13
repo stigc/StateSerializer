e6a4d0dcc6ba1ad21dac092a5e6b9a4c6d93db36
stigc <stig@stigc.dk>
2017-07-13 09:41:48 +0200

    1. version

---
+++ StateSerializer/StateSerializer.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace StateSerializerNs
+{
+    public class StateSerializer
+    {
+        private int nextId;
+        Dictionary<object, int> dic;
+        XmlWriter xw;
+
+        public int MaxListElementsToSerialize { get; set; }
+        List<string> NamespacesToIgnore = new List<string>();
+
+        public String ToXml(Object root)
+        {
+            nextId = 0;
+            dic = new Dictionary<object, int>(new IdentityEqualityComparer());
+
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "  ",
+                NewLineChars = "\r\n",
+                NewLineHandling = NewLineHandling.Replace
+            };
+
+            var sb = new StringBuilder();
+
+            using (xw = XmlWriter.Create(sb, settings))
+            {
+                PrintClassRecursive(root, 0, "Root");
+            }
+
+            return sb.ToString();
+        }
+
+        public void IgnoreNamspace(String ns)
+        {
+            NamespacesToIgnore.Add(ns);
+        }
+
+        private string getIdTag(object value)
+        {
+            if (value == null)
+                return null;
+
+            //strings and value-types should not have ID tag
+            if (value.GetType().IsValueType || value is string)
+                return null;
+
+            if (dic.ContainsKey(value) == false)
+            {
+                nextId++;
+                dic.Add(value, nextId);
+            }
+
+            return dic[value].ToString();
+        }
+
+        private void PrintClassRecursive(object current, int level, String nodeName)
+        {
+            xw.WriteStartElement(nodeName);
+
+            if (current != null)
+                xw.WriteAttributeString("Type", current.GetType().Name);
+
+            var hasBeenSerializedBefore = WriteIdTagAsAttribute(current);
+
+            if (current.ShouldNotBeTraversed(NamespacesToIgnore))
+            {
+                xw.WriteString(current.ToXmlValueStr());
+                xw.WriteEndElement();
+                return;
+            }
+
+            if (hasBeenSerializedBefore)
+            {
+                xw.WriteAttributeString("HasBeenSerializedBefore", "true");
+                xw.WriteEndElement();
+                return;
+            }
+
+            foreach (var field in GetFieldsRecursive(current.GetType()))
+            {
+                var value = field.GetValue(current);
+                var list = value.AsIEnumerable();
+
+                if (list != null)
+                {
+                    xw.WriteStartElement(field.NameToStr());
+                    xw.WriteAttributeString("Type", field.FieldType.Name);
+                    WriteIdTagAsAttribute(value);
+                    foreach (var arrayValue in value.AsIEnumerable())
+                    {
+                        PrintClassRecursive(arrayValue, level + 1, "item");
+                    }
+                    xw.WriteEndElement();
+                }
+                else
+                {
+                    if (value.ShouldNotBeTraversed(NamespacesToIgnore))
+                    {
+                        xw.WriteStartElement(field.NameToStr());
+                        xw.WriteAttributeString("Type", field.FieldType.Name);
+                        WriteIdTagAsAttribute(value);
+                        xw.WriteString(value.ToXmlValueStr());
+                        xw.WriteEndElement();
+                    }
+                    else
+                        PrintClassRecursive(value, level + 1, field.NameToStr());
+                }
+            }
+
+            xw.WriteEndElement();
+        }
+
+        private bool WriteIdTagAsAttribute(object value)
+        {
+            if (value == null)
+                return false;
+
+            bool isKnown = dic.ContainsKey(value);
+
+            var id = getIdTag(value);
+
+            if (id != null)
+                xw.WriteAttributeString("Id", id);
+
+            return isKnown;
+        }
+
+        private List<FieldInfo> GetFieldsRecursive(Type type)
+        {
+            if (NamespacesToIgnore.Any(ns => type.Namespace.StartsWith(ns)))
+                return new List<FieldInfo>();
+
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly | BindingFlags.Static)
+                .ToList();
+
+            if (type.BaseType == null)
+                return fields;
+
+            fields.AddRange(GetFieldsRecursive(type.BaseType));
+
+            return fields.OrderBy(x => x.Name)
+                .ThenBy(x => x.MemberType)
+                .ToList();
+        }
+    }
+
+    class IdentityEqualityComparer : IEqualityComparer<object>
+    {
+        public bool Equals(object o1, object o2)
+        {
+            return Object.ReferenceEquals(o1, o2);
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    static class ExtensionMehtods
+    {
+        public static bool ShouldNotBeTraversed(this object value, List<string> namespacesToIgnore)
+        {
+            return value == null
+                        || value is string
+                        || value.GetType().IsValueType
+                        || value is Type
+                        || value is System.Reflection.Pointer
+                        || value.GetType().BaseType == typeof(MulticastDelegate)
+                        || namespacesToIgnore.Any(x => value.GetType().Namespace.StartsWith(x));
+        }
+
+        public static string ToXmlValueStr(this object value)
+        {
+            if (value == null)
+                return null;
+
+            return value.ToString().RemoveInvalidXMLChars();
+        }
+
+        public static IEnumerable AsIEnumerable(this object value)
+        {
+            if (value is string)
+                return null;
+            if (value is XmlDocument)
+                return null;
+            return value as IEnumerable;
+        }
+
+        public static string NameToStr(this FieldInfo value)
+        {
+            var name = value.Name;
+
+            if (string.IsNullOrWhiteSpace(name))
+                return "noname";
+
+            name = new String(value.Name.Where(Char.IsLetter).ToArray());
+
+            if (string.IsNullOrWhiteSpace(name))
+                return "noname";
+
+            return name;
+        }
+
+        private static Regex _invalidXMLChars = new Regex(
+                @"(?<![\uD800-\uDBFF])[\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F\uFEFF\uFFFE\uFFFF]",
+                RegexOptions.Compiled);
+
+        public static string RemoveInvalidXMLChars(this string text)
+        {
+            if (string.IsNullOrEmpty(text)) return "";
+            return _invalidXMLChars.Replace(text, "");
+        }
+    }
+}
---
+++ StateSerializer/Tests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace StateSerializerNs.Tests
+{
+    class Tests
+    {
+        [Test]
+        public void AsIEnumerableShouldWork()
+        {
+            Assert.IsNull("abc".AsIEnumerable());
+            Assert.IsNull(1.AsIEnumerable());
+            Assert.IsNull(new XmlDocument().AsIEnumerable());
+
+            Assert.IsNotNull(new int[0].AsIEnumerable());
+            Assert.IsNotNull(new HashSet<string>().AsIEnumerable());
+            Assert.IsNotNull(new Dictionary<int, int>().AsIEnumerable());
+            Assert.IsNotNull(new ArrayList().AsIEnumerable());
+            Assert.IsNotNull(new Stack().AsIEnumerable());
+        }
+
+        [Test]
+        public void SerializerTest1()
+        {
+            var ss = new StateSerializer();
+            ss.IgnoreNamspace("System.Xml");
+
+            var str = ss.ToXml(new MyClass());
+            Console.WriteLine(str);
+        }
+    }
+
+
+    class MyBasClass : XmlDocument
+    {
+        protected string[] arrStrings = new string[]{ "a", "b", "c" };
+    }
+
+    class MyClass : MyBasClass
+    {
+        static DateTime date = DateTime.Now;
+        private Type myTypeField = typeof(string);
+
+        public XmlDocument[] arrayXml;
+        public int Id { get; set; } = 1;
+        public MyClass SelfReference { get; set; }
+        public object NullReference { get; set; }
+        public MyClass()
+        {
+            var xml = new XmlDocument();
+
+            this.SelfReference = this;
+            this.arrayXml = new XmlDocument[] { xml , xml, null };
+        }
+    }
+}
